f1523544050dbf87f9d6ee0bc2e85b7b2a5ebbdf
masterUNG <phrombutr@gmail.com>
2016-07-18 15:36:15 +0700

    Intent Main Success

--- app/src/main/java/rmutt/sorawith/suradech/sensorsystemdesign/MainActivity.java
+++ app/src/main/java/rmutt/sorawith/suradech/sensorsystemdesign/MainActivity.java
@@ -1,13 +1,55 @@
 package rmutt.sorawith.suradech.sensorsystemdesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView switchImageView, carParkImageView,
+            checkTempImageView, voltImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        switchImageView = (ImageView) findViewById(R.id.imageView2);
+        carParkImageView = (ImageView) findViewById(R.id.imageView3);
+        checkTempImageView = (ImageView) findViewById(R.id.imageView4);
+        voltImageView = (ImageView) findViewById(R.id.imageView5);
+
+        //Image Controller
+        switchImageView.setOnClickListener(this);
+        carParkImageView.setOnClickListener(this);
+        checkTempImageView.setOnClickListener(this);
+        voltImageView.setOnClickListener(this);
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.imageView2:
+                startActivity(new Intent(MainActivity.this, SwitchActivity.class));
+                break;
+            case R.id.imageView3:
+                startActivity(new Intent(MainActivity.this, CarParkActivity.class));
+                break;
+            case R.id.imageView4:
+                startActivity(new Intent(MainActivity.this, TempActivity.class));
+                break;
+            case R.id.imageView5:
+                startActivity(new Intent(MainActivity.this, VoltActivity.class));
+                break;
+        }
+
+    }   // onClick
+
+}   // Main Class

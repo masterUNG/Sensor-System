7d10a167d52d48d8589e9ab53965a9c60a1e43b2
masterUNG <phrombutr@gmail.com>
2016-07-18 14:47:43 +0700

    Design UI Main Finish

---
+++ app/src/main/java/rmutt/sorawith/suradech/sensorsystemdesign/CarParkActivity.java
@@ -0,0 +1,13 @@
+package rmutt.sorawith.suradech.sensorsystemdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarParkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_park);
+    }
+}
